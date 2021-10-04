d82a4d5e876c1e0c0457baae761a151f2d3afb4f
Huayi(Justin) Geng <justingenghy@gmail.com>
2021-10-03 20:30:38 -0400

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by huayi geng on 2021-10-03.
+//  Copyright © 2021 huayi geng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
